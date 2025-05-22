325342ff5373ed2600e9ee70e2412b617456d56c
Ruthgyeul <wingd4705@gmail.com>
2025-05-22 11:03:06 +0900

    Initial Commit

---
+++ src/main/java/ruthgyeul/ruthapi/RuthApiApplication.java
@@ -0,0 +1,13 @@
+package ruthgyeul.ruthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruthgyeul/ruthapi/RuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ruthgyeul.ruthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
